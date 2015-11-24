c16f5d192455ee789d4bf2b792f48cb9f7189caf
Sam Shelton <sam@shelt.ca>
2015-11-23 22:11:01 -0600

    Implement removal if no flair within 60s

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import praw
 import requests
+import time
 
 # Account settings (private)
 USERNAME = ''
@@ -11,10 +12,18 @@ CLIENT_SECRET = ''
 REDIRECT_URI = 'http://127.0.0.1:65010/authorize_callback'
 
 # Configuration Settings
-USER_AGENT = "JessicaJones UndeleteFlairedPosts"
+USER_AGENT = "UndeleteFlairedPosts for JessicaJones"
 SUBREDDIT = "JessicaJones"
-AUTH_TOKENS = ["identity","history", "read", "modposts"]
+AUTH_TOKENS = ["identity","history", "read", "modposts", "submit", "edit"]
+MAX_TIME_FLAIRLESS = 60 # Seconds. Amount of time a post can be without flair (to allow user time to set it)
+REMOVAL_IDENTIFIER = "[UNFLAIRED]:#" # Used to identify comments removed for being unflaired. I believe this is cleaner than keeping a list.
+REMOVAL_MESSAGE = REMOVAL_IDENTIFIER +"\n\n"+ """
+This post has been temporarily and automatically removed because it has no assigned flair. It will be unremoved once a flair is assigned.
 
+------------------------------
+
+If you believe this is an error, contact /u/vbats1.
+"""
 
 def get_access_token():
     response = requests.post("https://www.reddit.com/api/v1/access_token",
@@ -29,16 +38,39 @@ def get_praw():
     r.set_access_credentials(set(AUTH_TOKENS), get_access_token())
     return r
 
+# Determines if the bot has already dealt with this post WRT REMOVAL_IDENTIFIER
+def already_replied(post):
+    for comment in post.comments:
+        if comment.author is None:
+            continue
+        if comment.author.name.lower() == USERNAME.lower() and comment.body.startswith(REMOVAL_IDENTIFIER):
+            return comment
+    return None
+
 def main(r):
-    sub = r.get_subreddit(SUBREDDIT)
-    for post in sub.get_spam(limit=None):
-        # TODO if post has a comment saying the post was removed for being unflaired
-        if post.link_flair_text is not None:
-            post.approve()
-    exit()
+    while True:
+        sub = r.get_subreddit(SUBREDDIT)
+        
+        # Delete unflaired posts
+        for post in sub.get_new(limit=None):
+            if post.link_flair_text is None:
+                diff = int(time.time()) - post.created_utc
+                if diff > MAX_TIME_FLAIRLESS and not already_replied(post):
+                    post.add_comment(REMOVAL_MESSAGE)
+                    post.remove()
+        
+        # Undelete flaired posts
+        for post in sub.get_spam(limit=None):
+            if post.link_flair_text is not None:
+                my_comment = already_replied(post)
+                if my_comment is not None: # We know it was removed for being unflaired
+                    post.approve()
+                    my_comment.delete()
+        time.sleep(10)
 
 if __name__ == "__main__":
-    try:
-        main(get_praw())
-    except praw.errors.OAuthInvalidToken:
-        print("OAuth token expired.")
+    while True:
+        try:
+            main(get_praw())
+        except praw.errors.OAuthInvalidToken:
+            print("OAuth token expired.")
